3ec94f9c797bbd6355845be903e8028097fb752f
Nick Reed <niccor83@gmail.com>
2019-08-12 00:07:08 -0600

    added styling in style.css, added README content

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # GifTastic
 App that pulls gifs from Giphy and displays them after clicking a button
+
+What the project does: This app was designed to allow the user to pull up food or drink related gifs. There are several predefined categories, or the user can add their own. 
+
+Why the project is useful: This project is helpful in teaching the coder how to work with Ajax, and API's. The coder should learn how to make an ajax call to an API and recieve data back. 
+
+How users can get started with the project: Play around with the app to become familiar with what it does, then try and recreate the code.
+
+Where users can get help with your project: niccor83@gmail.com
+
+Who maintains and contributes to the project: Nick Reed
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 //array of buttons to display on when page is loaded
-var buttonArray = ["Tacos", "Burritos", "Diet Coke", "Pizza", "Mountain Dew", "Soup", "Sprite", "Hamburger", "Beer", "Hotdogs", "French Fries", "Milk Shake", "Pepsi"];
+var buttonArray = ["Tacos", "Burritos", "Diet Coke", "Pizza", "Mountain Dew", "Soup", "Sprite", "Hamburger", "Beer", "Hot Dogs", "French Fries", "Milk Shake", "Pepsi"];
 
 //creates button in html
 function renderButtons(){
@@ -7,7 +7,7 @@ function renderButtons(){
 
     for(var i = 0; i < buttonArray.length; i++){
        
-        var a = $("<button>");
+        var a = $('<button class="btn btn-light">');
         a.addClass("gif-btn");
         a.attr("data-name", buttonArray[i]);
         a.text(buttonArray[i]);
@@ -51,6 +51,7 @@ $("button").on("click", function () {
             console.log(response);
             // Storing an array of results in the results variable
             var results = response.data;
+            console.log(results);
 
             // Looping over every result item
             for (var i = 0; i < results.length; i++) {
@@ -58,7 +59,7 @@ $("button").on("click", function () {
                 // Only taking action if the photo has an appropriate rating
                 if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
                     // Creating a div for the gif
-                    var gifDiv = $("<div>");
+                    var gifDiv = $('<div>');
 
                     // Storing the result item's rating
                     var rating = results[i].rating;
@@ -67,11 +68,15 @@ $("button").on("click", function () {
                     var p = $("<p>").text("Rating: " + rating);
 
                     // Creating an image tag
-                    var gifImage = $("<img>");
+                    var gifImage = $('<img id="gif-img">');
 
                     // Giving the image tag an src attribute of a proprty pulled off the
                     // result item
                     gifImage.attr("src", results[i].images.fixed_height.url);
+                    gifImage.attr("data-still", results[i].images.fixed_height_still.url);
+                    gifImage.attr("data-animate", results[i].images.fixed_height.url);
+                    gifImage.attr("data-state", "still");
+
 
                     // Appending the paragraph and personImage we created to the "gifDiv" div we created
                     gifDiv.append(p);
@@ -82,4 +87,20 @@ $("button").on("click", function () {
                 }
             }
         });
+});
+
+$("#gif-img").on("click", function () {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    console.log("you clicked me")
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
 });
